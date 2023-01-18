1ce271e22f23b3026d87b30cc9268b1e16c72630
dianeferreira <dianeferreiraa@outlook.com>
2023-01-18 16:53:36 -0300

    Projeto finalizado

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from config.database import cursor, connecting
 app = Flask(__name__)
 
 
+@app.route("/", methods=['GET'])
+def read_all():
+    sql = "SELECT * FROM cadastro;"
+    cursor.execute(sql)
+    cadastro = cursor.fetchall()
+    return cadastro
+
+
 @app.route("/cadastro", methods=['POST'])
 def create():
     body = request.json
@@ -19,3 +27,26 @@ def create():
                    data_nascimento, email, senha))
     connecting.commit()
     return {"message": "Cadastro efetuado com sucesso"}
+
+
+@app.route("/delete/<id>", methods=['DELETE'])
+def delete(id):
+    sql = "DELETE FROM cadastro WHERE id = %s"
+    cursor.execute(sql, (id))
+    connecting.commit()
+    return {"mesage": "Usuário deletado com sucesso"}
+
+
+@app.route("/update/<id>", methods=["PUT"])
+def update(id):
+    body = request.json
+    email = body.get("email")
+    senha = body.get("senha")
+    sql = "UPDATE cadastro SET email = %s, senha = %s WHERE id = %s"
+    try:
+        cursor.execute(sql, (email, senha, id))
+        connecting.commit()
+        return {"message": "Dados atualizados com sucesso"}
+
+    except:
+        return {"message": "Falha ao atualizar os dados"}
--- config/database.py
+++ config/database.py
@@ -4,3 +4,4 @@ connecting = psycopg2.connect(
     dbname="teste", user="postgres", password="postgres", host="localhost")
 
 cursor = connecting.cursor()
+
